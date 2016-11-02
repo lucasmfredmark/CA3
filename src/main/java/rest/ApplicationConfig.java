ebc67f49901ff0a076783bfc915252ef53662357
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2016-11-02 10:42:59 +0100

    Deleted demo REST services

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -3,39 +3,28 @@ package rest;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-
 @javax.ws.rs.ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
-  @Override
-  public Set<Class<?>> getClasses() {
-    Set<Class<?>> resources = new java.util.HashSet<>();
-    addRestResourceClasses(resources);
-    return resources;
-  }
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
 
-  /**
-   * Do not modify addRestResourceClasses() method.
-   * It is automatically populated with
-   * all resources defined in the project.
-   * If required, comment out calling this method in getClasses().
-   */
-  private void addRestResourceClasses(Set<Class<?>> resources) {
-    resources.add(httpErrors.GenericExceptionMapper.class);
-    resources.add(httpErrors.NotFoundExceptionMapper.class);
-    resources.add(rest.Admin.class);
-    resources.add(rest.All.class);
-    resources.add(rest.FollowList.class);
-    resources.add(rest.Pokemon.class);
-    resources.add(rest.Team.class);
-    resources.add(rest.User.class);
-    resources.add(rest.UserResource.class);
+    /**
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(httpErrors.GenericExceptionMapper.class);
+        resources.add(httpErrors.NotFoundExceptionMapper.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.Login.class);
         resources.add(security.NotAuthorizedExceptionMapper.class);
         resources.add(security.RolesAllowedFilter.class);
-   
-   
-  }
-  
+    }
+
 }
