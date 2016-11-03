6e2c784c5a38c2af5a4dc2a83553bcdce94ea58f
xboxm <you@example.com>
2016-11-03 15:53:28 +0100

    Implemented pokeapijs which can fetch a pokemon by id from pokeapico so we can get detailed information about each pokemon we want

--- src/main/webapp/app/external/pokeapi.js
+++ src/main/webapp/app/external/pokeapi.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 'use strict';
 
@@ -18,28 +13,22 @@ angular.module('myApp.pokeapi', ['ngRoute'])
 
         .controller('pokeApiCtrl', ['pokeApiFactory', function (pokeApiFactory) {
                 var self = this;
-                self.pokeApiTest = pokeApiFactory.factoryTest();
-//                self.getPokemonById = function (id) {
-//                    console.log("Getting pokemon by id: " + id);
-//                    pokeApiFactory.getPokemonById(id)
-//                            .then(function (response) {
-//                                console.log("Response is: " + response);
-//                                self.pokemon = response.data;
-//                                console.log(self.countries_all);
-//                            }, function (error) {
-//                                self.status = 'Unable to load customer data: ' + error.message;
-//                            });
-//                };
+                self.controllerTest = "Hello from controller";
+                self.pokeApiFactoryTest = pokeApiFactory.factoryTest;
+                self.getPokemonById = pokeApiFactory.getPokemonById(130).then(function (response) {
+                                self.pokemon = response.data;
+                            }, function (error) {
+                                self.status = 'Unable to load customer data: ' + error.message;
+                            });
             }])
-
-        .factory('pokeApiFactory' [function () {
-            var pokeapi = {};
-//            var baseUrl = "http://pokeapi.com/api/v2/pokemon/";
-            pokeapi.factoryTest = function () {
-                return "Response from pokeapi.getPokemonById";
-            };
-//            pokeapi.getPokemonById = function (id) {
-//                return $http.get(baseUrl + id);
-//            };
-            return pokeapi;
-        }]);
+        .factory('pokeApiFactory', ['$http', function ($http) {
+                var factory = {};
+                var baseUrl = "http://pokeapi.co/api/v2/pokemon/";
+                factory.factoryTest = "Hello from factory";
+                factory.getPokemonById = function(id) {
+//                    console.log($http.get(baseUrl + id));
+//                    return "Get pokemon with id: " + id + " & url: " + baseUrl;
+                    return $http.get(baseUrl + id);
+                };
+                return factory;
+            }]);
