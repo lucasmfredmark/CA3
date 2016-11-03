5e58eeca00c8c99b4c565af60fffc4a332d92021
xboxm <you@example.com>
2016-11-03 16:31:03 +0100

    Implemented functionality to make multiple RESTFUL get calls and display a table of random pokemon in pokeapijs

--- src/main/webapp/app/external/pokeapi.js
+++ src/main/webapp/app/external/pokeapi.js
@@ -13,13 +13,27 @@ angular.module('myApp.pokeapi', ['ngRoute'])
 
         .controller('pokeApiCtrl', ['pokeApiFactory', function (pokeApiFactory) {
                 var self = this;
+                var count = 0;
                 self.controllerTest = "Hello from controller";
                 self.pokeApiFactoryTest = pokeApiFactory.factoryTest;
+                self.pokemonList = [];
+                self.pokemonToGet = [Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1)];
                 self.getPokemonById = pokeApiFactory.getPokemonById(130).then(function (response) {
                                 self.pokemon = response.data;
                             }, function (error) {
                                 self.status = 'Unable to load customer data: ' + error.message;
                             });
+                self.pokemonToGet.forEach(function(entry) {
+//                    console.log(entry);
+                    self.pokemonList[count] = pokeApiFactory.getPokemonById(entry).then(function (response) {
+                                console.log(response.data);
+                                return response.data;
+                            }, function (error) {
+                                return 'Unable to load customer data: ' + error.message;
+                            });;
+                    count++;
+                });
+                console.log(self.pokemonList);
             }])
         .factory('pokeApiFactory', ['$http', function ($http) {
                 var factory = {};
