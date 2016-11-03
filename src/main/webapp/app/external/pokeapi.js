fdd7608bc9eff09ef2404b1f3f6e3d5d21918214
xboxm <you@example.com>
2016-11-03 16:56:50 +0100

    Made some graphical changes to give the webapp some personality

--- src/main/webapp/app/external/pokeapi.js
+++ src/main/webapp/app/external/pokeapi.js
@@ -14,18 +14,22 @@ angular.module('myApp.pokeapi', ['ngRoute'])
         .controller('pokeApiCtrl', ['pokeApiFactory', function (pokeApiFactory) {
                 var self = this;
                 var count = 0;
+                
                 // Module tests
                 self.controllerTest = "Hello from controller";
                 self.pokeApiFactoryTest = pokeApiFactory.factoryTest;
+                
                 //List containing the 6 IDS of the pokemon to display. Empty to start. pokemonToGet sets 6 random integers in 
                 self.pokemonList = [];
                 self.pokemonToGet = [Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1), Math.floor((Math.random() * 150) + 1)];
+                
                 // Method gets a single pokemon by id. Hard coded example below
                 self.getPokemonById = pokeApiFactory.getPokemonById(130).then(function (response) {
                                 self.pokemon = response.data;
                             }, function (error) {
-                                self.status = 'Unable to load customer data: ' + error.message;
+                                self.status = 'Unable to load pokemon data: ' + error.message;
                             });
+                
                 // for each index in array, call getPokemonById(var id) where var id = random generated integer from pokemonToGet 
                 self.pokemonToGet.forEach(function(entry) {
                     //Add a pokemon to list which is a object from pokeapi
@@ -33,7 +37,7 @@ angular.module('myApp.pokeapi', ['ngRoute'])
                                 console.log(response.data);
                                 return response.data;
                             }, function (error) {
-                                return 'Unable to load customer data: ' + error.message;
+                                return 'Unable to load pokemon data: ' + error.message;
                             });;
                     // Increases array index we add to        
                     count++;
